2fe1b6916b33f0851b1328ab97c03d2e596b5d09
kbc11a07 <kbc11a07@kbc11a07-PC>
2013-11-12 11:41:30 +0900

    factory2

--- FactoryMethod/src/masahiro/tamaru/kj/kbc/Main.java
+++ FactoryMethod/src/masahiro/tamaru/kj/kbc/Main.java
@@ -3,7 +3,21 @@ package masahiro.tamaru.kj.kbc;
 public class Main {
 
 	public static void main(String[] args) {
-		WindInstrumentProduct 
+		SaxophoneCreator sc = new SaxophoneCreator();
+		TrumpetCreator tc = new TrumpetCreator();
+
+		WindInstrumentProduct ins = sc.create();
+		WindInstrumentProduct ins2 = tc.create();
+
+		ins.setPlate("フォン");
+		ins2.setPlate("トラン");
+
+		ins.printPlate();
+		ins.play();
+		ins2.printPlate();
+		ins2.play();
+
+
 	}
 
 }
